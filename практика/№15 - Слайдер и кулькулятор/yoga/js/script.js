e500b5b29ad8dd95b952d6de7236bdf14cf89552
twogog <you@example.com>
2019-11-19 17:43:10 +0600

    Сделали калькулятор со слайдером

---
+++ практика/№15 - Слайдер и кулькулятор/yoga/js/script.js
@@ -0,0 +1,287 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+
+    });
+
+    // Timer 
+
+    let deadline = '2018-11-21';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num){
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
+
+    //Модальное окно
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+    
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = 'auto';
+    });
+
+
+    let modalbut = document.querySelectorAll('.description-btn');
+
+    modalbut.forEach(function callback(currentValue, i) {
+        modalbut[i].addEventListener('click', function() {
+            overlay.style.display = 'block';
+            modalbut[i].classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    //Форма отправки без JSON и с ним  через Promise       input в форме должен содержать name!!!!!
+    
+    let message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        formDown = document.getElementById('form'),
+        input = document.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+
+    function sendForm(elem) {
+        elem.addEventListener('submit', function(event) {
+            event.preventDefault();
+            elem.appendChild(statusMessage);
+            let formData = new FormData(elem);                 //Все, что ответил пользователь в форме
+
+            function postData(data) {
+                return new Promise(function(resolve, reject) {
+                    let request = new XMLHttpRequest();
+
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                    // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');   // JSON
+
+                    // let obj = {};                                      //нужно для JSON
+                    // formData.forEach(function (value, key) {           //formData преобразуется в читабельный вид
+                    //     obj[key] = value;                              //нужно не только для JSON
+                    // });
+
+                    // let json = JSON.stringify(obj);                    //преобразуем объект в JSON формат
+
+                    request.send(formData);
+                    // request.send(json);                                //JSON отправляется на сервер
+
+                    request.addEventListener('readystatechange', function() {
+                        if (request.readyState < 4) {
+                            statusMessage.innerHTML = message.loading;
+                        } else if (request.readyState === 4 && request.status == 200) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                    
+                });    
+            }
+
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+
+            postData(formData)
+                .then(() => statusMessage.innerHTML = message.success)
+                .catch(() => statusMessage.innerHTML = message.failure);
+
+        });
+    }
+
+    sendForm(form);
+    sendForm(formDown);
+
+    //Слайдер
+
+    let slideIndex = 1,                 // текущий слайд
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if (n > slides.length) {            //листаем вперед
+            slideIndex = 1;
+        }
+
+        if (n < 1) {                        //листаем назад
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1] ) {
+                currentSlide(i);
+            }
+        }
+    });
+
+    //Калькулятор
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restdays = document.querySelectorAll('.counter-block-input')[1],
+        allinputs = document.querySelectorAll('.counter-block-input'),
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerHTML = 0;
+
+    function checkInputs() {
+        allinputs.forEach(function(a, b) {
+            if (allinputs[b].value == '' || allinputs[b].value == 0){
+                totalValue.innerHTML = 0;
+            }
+        });
+    }
+
+    persons.addEventListener('input', function() {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (restdays.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+        
+        checkInputs();
+    });
+
+    restdays.addEventListener('input', function() {
+        daysSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (personsSum.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+
+        checkInputs();
+    });
+
+    place.addEventListener('change', function() {
+        if (restdays.value == '' && persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
+});
+
